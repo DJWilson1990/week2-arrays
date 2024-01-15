618f1d10458a5accc6e26907b2f81727a7244171
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-15 14:29:15 +0000

    arrays task

--- arrays.js
+++ arrays.js
@@ -1,40 +1,41 @@
-// console.log("hello Dan");
+console.log("hello Dan");
 
-// const blogPost = ["My first Blog", "My second blog", "My third blog"];
+const blogPost = ["My first Blog", "My second blog", "My third blog"];
 
-// console.log(blogPost); //will tell you length of array (3) and what they are. lenght is 3 but last array is 2. LENGHT 1,2,3 ARRAYS 0,1,2
-// console.log(blogPost[0]); //selects first string. Arrays start at 0
-// console.log(blogPost[1]); // selects second string("My second blog")
-// console.log(blogPost[2]);
+console.log(blogPost); //will tell you length of array (3) and what they are. lenght is 3 but last array is 2. LENGHT 1,2,3 ARRAYS 0,1,2
+console.log(blogPost[0]); //selects first string. Arrays start at 0
+console.log(blogPost[1]); // selects second string("My second blog")
+console.log(blogPost[2]);
 
-// blogPost[3] = "My forth blog";
-// console.log(blogPost[3]);
+blogPost[3] = "My forth blog";
+console.log(blogPost[3]);
 
-// const favFood = ["Pasta", "Pizza", "Japanese cuisine", "Curries"];
+const favFood = ["Pasta", "Pizza", "Japanese cuisine", "Curries"];
 
-// console.log(favFood[2]); //japanese
-// console.log(favFood[0]); //pasta
-// console.log(favFood[3]); //curries
-// console.log(favFood[1]); //pizza
+console.log(favFood[2]); //japanese
+console.log(favFood[0]); //pasta
+console.log(favFood[3]); //curries
+console.log(favFood[1]); //pizza
 
-// const favHobbies = ["Running", "Skiing", "Kitesurfing"];
+const favHobbies = ["Running", "Skiing", "Kitesurfing"];
 
-// console.log(favHobbies); // console shoes '(3) ['Running', 'Skiing', 'Kitesurfing']
-// console.log(favHobbies[0]); //running
-// console.log(favHobbies[1]); //skiing
-// console.log(favHobbies[2]); //kitesurfing
+console.log(favHobbies); // console shoes '(3) ['Running', 'Skiing', 'Kitesurfing']
+console.log(favHobbies[0]); //running
+console.log(favHobbies[1]); //skiing
+console.log(favHobbies[2]); //kitesurfing
 
-// const favColours = ["Orange", "Purple"];
+const favColours = ["Orange", "Purple"];
 
-// console.log(favColours[1]);
-// console.log(favColours[0]);
+console.log(favColours[1]);
+console.log(favColours[0]);
 
-// const favNumbers = [4, 8, 10];
+const favNumbers = [4, 8, 10];
 
-// console.log(favNumbers[0]);
-// console.log(favNumbers[2]);
-// console.log(favNumbers[1]);
+console.log(favNumbers[0]);
+console.log(favNumbers[2]);
+console.log(favNumbers[1]);
 
+//length -
 const myFavThings = ["food", "sport", 10]; //can put strings, numbers and boolean values in arrays!
 
 console.log(myFavThings[2]); // console shows 10
